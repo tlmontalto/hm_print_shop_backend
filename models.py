101dbf2c4f0a988ce8568d642367523665f75600
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-07-02 14:37:02 -0400

    changed else to the same information as in the if side

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ from playhouse.db_url import connect
 if 'ON_HEROKU' in os.environ:
     DATABASE = PostgresqlDatabase('dfgaaipjmoouah', user='yomnzeafsjhioq', password='91137a853bf3868ac2990020b452c5b5146fd29deac3ac800b85d6d8ecc06100', host='ec2-54-147-93-73.compute-1.amazonaws.com', port=5432)
 else:
-    DATABASE = PostgresqlDatabase('hmprintshop')
+    DATABASE = PostgresqlDatabase('dfgaaipjmoouah', user='yomnzeafsjhioq', password='91137a853bf3868ac2990020b452c5b5146fd29deac3ac800b85d6d8ecc06100', host='ec2-54-147-93-73.compute-1.amazonaws.com', port=5432)
 
 class HMPUser(UserMixin, Model):
     username = CharField(unique=True)
