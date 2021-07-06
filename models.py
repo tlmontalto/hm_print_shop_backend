d63232433e3e9a0dcb92bed81b3f23f998018088
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-07-06 11:27:50 -0400

    added boto3 functions

--- models.py
+++ models.py
@@ -6,7 +6,15 @@ from flask_login import UserMixin
 from playhouse.db_url import connect
 
 import boto3
+import pandas
+
+
 s3 = boto3.resource('s')
+
+S3_BUCKET = os.environ.get('hm-stl-files')
+S3_KEY = os.environ.get('AKIA5UDGJH2NGSHYTZVQ')
+S3_SECRET = os.environ.get('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')
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 if 'ON_HEROKU' in os.environ:
     DATABASE = PostgresqlDatabase('dfgaaipjmoouah', user='yomnzeafsjhioq', password='91137a853bf3868ac2990020b452c5b5146fd29deac3ac800b85d6d8ecc06100', host='ec2-54-147-93-73.compute-1.amazonaws.com', port=5432)
